4803a38ab00e2066dfc9cabe98a077a4944a0abf
kshitij.pandey <kshitij.pandey@venturepact.com>
2022-12-17 17:19:42 +0530

    fix

--- controllers/common.js
+++ controllers/common.js
@@ -10,7 +10,7 @@ exports.sendMail = async function ({name,email, body, subject}) {
         to: email,
         from: {
             name: `Divesh`,
-            email: 'tom@nyeglommenrevisjon.com'
+            email: 'divesh.24747@lpu.co.in'
         }, 
         substitutions: {
             name: name,
